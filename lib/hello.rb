f9a249732337f607efd096664d363928d9036dba
Andres Carvajal <jc1993box@hotmail.com>
2020-04-25 03:09:05 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 array = ["Tim", "Tom", "Jim"]
+<<<<<<< HEAD
 def hello_t(array)
   if block_given?
   name = 0
@@ -14,8 +15,17 @@ def hello_t(array)
 end
 
 
+=======
+
+
+def hello_t(array)
+>>>>>>> 6196cde9a6f9d4293c03c313c08f1c84b0c83523
  hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
 end
+<<<<<<< HEAD
+=======
+end
+>>>>>>> 6196cde9a6f9d4293c03c313c08f1c84b0c83523
